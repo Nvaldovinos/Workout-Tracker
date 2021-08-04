168cad728f818eba38a00e058f7a89477bce2a9b
Naomi <n.valdovinos74@gmail.com>
2021-08-04 12:34:22 -0400

    added routes and i think its finished???

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,45 @@
 const router = require('express').Router();
 const Workout = require('../models/workout.js')
 
+//route to grab all the workouts and populate the dashboard
+router.get('/api/workouts', (req, res) => {
+    Workout.find({})
+    then(dbWorkout =>{
+        res.json(dbWorkout)
+    })
+    .catch(err =>{
+        res.status(400).json(err)
+    });
+});
+
+
+// router.get('/api/workouts', (req, res) => {
+//     Workout.aggregate(
+//         //get total duration of the workouts.
+//     )
+//     .then(dbWorkout =>{
+//         res.json(dbWorkout)
+//     })
+//     .catch(err =>{
+//         res.status(400).json(err)
+//     });
+// })
+
+
+//route to get the workouts within their range
+router.get('/api/workouts/range', (req, res) =>{
+    Workout.find({})
+    then(dbWorkout =>{
+        res.json(dbWorkout)
+    })
+    .catch(err =>{
+        res.status(400).json(err)
+    });
+});
 
 //gets workout.js from public folder 
-router.post('/api/workouts', (req, res) => {
-   Workout.create({})
+router.post('/api/workouts', ({ body }, res) => {
+   Workout.create(body)
    .then(dbWorkout =>{
        res.json(dbWorkout)
    })
@@ -14,10 +49,10 @@ router.post('/api/workouts', (req, res) => {
 });
   
 //route to update existing workouts
-router.put('/api/workouts/:id', (req, res) => {
-    Workout.findByIdAndUpdate(req.params.id, 
-     //second parameter should give information to update workout   
-        )
+router.put('/api/workouts/:id', async (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, {
+        $push: { exercises: req.body}
+    })
         .then(dbWorkout =>{
             res.json(dbWorkout)
         })
@@ -25,21 +60,6 @@ router.put('/api/workouts/:id', (req, res) => {
             res.status(400).json(err)
         });
 });
-
-//route to grab all the workouts and populate the dashboard
-router.get('/api/workouts', (req, res) => {
-    Workout.aggregate(
-        //get total duration of the workouts.
-    )
-    .then(dbWorkout =>{
-        res.json(dbWorkout)
-    })
-    .catch(err =>{
-        res.status(400).json(err)
-    });
-})
-
-//route to get the workouts within their range
 
 //route to delete workouts. router.delete
 
